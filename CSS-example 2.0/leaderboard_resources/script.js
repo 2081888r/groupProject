5b78a16b8a41ad51a435dc2717785a06d197e9f2
Dylan Lawrence <carlmccarlton64@gmail.com>
2016-03-11 18:44:31 +0000

    Added leaderboard page to the CSS template and created dummy pages

---
+++ CSS-example 2.0/leaderboard_resources/script.js
@@ -0,0 +1,12 @@
+function showTableSortButton2(){
+    document.getElementById("table_sort_button_2").style.visibility = "visible";
+}
+function hideTableSortButton2(){
+    document.getElementById("table_sort_button_2").style.visibility = "hidden";
+}
+function showTableSortButton3(){
+    document.getElementById("table_sort_button_3").style.visibility = "visible";
+}
+function hideTableSortButton3(){
+    document.getElementById("table_sort_button_3").style.visibility = "hidden";
+}
